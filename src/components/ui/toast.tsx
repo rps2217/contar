49b56d9f1b717d2f6f9075604a62be3eedfac80b
rolando pizarro <rolando.pizarro.salas@gmail.com>
2025-05-18 12:56:01 +0000

    Quizás las notificaciones deberían aparecer a pie de página para no ocultar el campo visible del input

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -16,7 +17,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed bottom-4 left-1/2 -translate-x-1/2 z-[100] flex w-auto max-w-[calc(100%-2rem)] sm:max-w-md flex-col-reverse gap-3 p-0",
       className
     )}
     {...props}
